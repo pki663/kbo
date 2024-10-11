a35b9b7f300c3f23f5c69bfbfda7ec18b5fa621d
Soohyeok Kim <sooo@unist.ac.kr>
2024-10-11 21:12:29 +0900

    SPO Game 5

--- preprocess_postseason.py
+++ preprocess_postseason.py
@@ -141,7 +141,7 @@ spo_fig.update_yaxes(range = [0, 1], fixedrange = True, tickformat = ',.3%')
 spo_fig.update_layout(title_text = '진출 확률', margin_l=10, margin_r=10, margin_b=10, margin_t=50, plot_bgcolor='#D9F2D0', paper_bgcolor="#DFDFDF")
 
 lg_gamewin = log5(76/142, 72/142)
-spo_result = [(0,0), (0,1), (1,1), (2,1), (2,2)]
+spo_result = [(0,0), (0,1), (1,1), (2,1), (2,2), (3,2)]
 spo_fig.add_traces([
     go.Scatter(
         name = 'LG', 
@@ -159,7 +159,7 @@ spo_fig.add_traces([
         for x, y in spo_result],
         text = ['{}승 {}패'.format(x[1], x[0]) for x in spo_result],
         mode = 'lines', line = {'color': team_color['KT'][0], 'width' : 3, 'dash': 'solid'}, marker = {'color': team_color['KT'][1], 'size': 3},
-        legendgroup = 'kt'
+        legendgroup = 'kt', visible = 'legendonly'
     )
 ])
 
@@ -180,7 +180,7 @@ for idx, (prev, next) in enumerate(zip(spo_result[:-1], spo_result[1:])):
                 sum([b for a, b in postseason_ratio(3, lg_gamewin, prev[0], prev[1]).items() if a[1] == 3]), 
                 sum([b for a, b in postseason_ratio(3, lg_gamewin, alt_next[0], alt_next[1]).items() if a[1] == 3])],
             mode = 'lines', line = {'color': team_color['KT'][0], 'width' : 3, 'dash': 'dash'}, marker = {'color': team_color['KT'][1], 'size': 3},
-            legendgroup = 'kt', hoverinfo='skip', showlegend= False
+            legendgroup = 'kt', hoverinfo='skip', showlegend= False, visible = 'legendonly'
         )
     ])
 
@@ -211,7 +211,7 @@ if (spo_result[-1][0] != 3) and (spo_result[-1][1] != 3):
                 sum([b for a, b in postseason_ratio(3, lg_gamewin, spo_result[-1][0] + 1, spo_result[-1][1]).items() if a[1] == 3])
             ],
             mode = 'lines', line = {'color': team_color['KT'][0], 'width' : 3, 'dash': 'dash'}, marker = {'color': team_color['KT'][1], 'size': 3},
-            legendgroup = 'kt', hoverinfo='skip', showlegend= False
+            legendgroup = 'kt', hoverinfo='skip', showlegend= False, visible = 'legendonly'
         ),
         go.Scatter(
             x = [len(spo_result) - 1, len(spo_result)],
@@ -220,7 +220,7 @@ if (spo_result[-1][0] != 3) and (spo_result[-1][1] != 3):
                 sum([b for a, b in postseason_ratio(3, lg_gamewin, spo_result[-1][0], spo_result[-1][1] + 1).items() if a[1] == 3])
             ],
             mode = 'lines', line = {'color': team_color['KT'][0], 'width' : 3, 'dash': 'dash'}, marker = {'color': team_color['KT'][1], 'size': 3},
-            legendgroup = 'kt', hoverinfo='skip', showlegend= False
+            legendgroup = 'kt', hoverinfo='skip', showlegend= False, visible = 'legendonly'
         )
     ])
 
@@ -253,42 +253,37 @@ style_data = {'text-align': 'center', 'padding': '3px'},
 style_table={'margin-left': 'auto', 'margin-right': 'auto', 'margin-top': '10px', 'margin-bottom': '10px', 'width': '100%', 'max-width': '600px', 'overflowX': 'auto'})
 
 # Semi-Playoff
-spo_initial = {'(LG-kt)': '시리즈 초기'}
-spo_initial.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items()})
-spo_initial['LG 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items() if x[0] == 3]), '.3%')
-spo_initial['kt 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items() if x[1] == 3]), '.3%')
+spo_dict = {'(LG-kt)': '확률'}
+spo_dict.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items()})
+spo_dict['LG 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items() if x[0] == 3]), '.3%')
+spo_dict['kt 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 0, 0).items() if x[1] == 3]), '.3%')
 
-spo_now = {'(LG-kt)': '현재'}
-spo_now.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, lg_gamewin, 2, 2).items()})
-spo_now['LG 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 2, 2).items() if x[0] == 3]), '.3%')
-spo_now['kt 진출'] = format(sum([y for x, y in postseason_ratio(3, lg_gamewin, 2, 2).items() if x[1] == 3]), '.3%')
-
-spo_probability = dash_table.DataTable([spo_initial, spo_now],
+spo_probability = dash_table.DataTable([spo_dict],
     [{'name': i, 'id': i} for i in ['(LG-kt)', '3-0', '3-1', '3-2', 'LG 진출', '0-3', '1-3', '2-3', 'kt 진출']],
     style_cell_conditional=[
-        {'if': {'column_id': ['3-0', '0-3', '1-3', '3-1']}, 'backgroundColor': '#C0C0C0'},
-        {'if': {'column_id': ['LG 진출', 'kt 진출']}, 'border-left': '2px solid black' , 'border-right': '4px solid black'},
-        {'if': {'column_id': ['(LG-kt)']}, 'border-right': '4px solid black'}],
+    {'if': {'column_id': ['3-2', 'LG 진출']}, 'fontWeight': 'bold', 'backgroundColor': '#BEF5CE'},
+    {'if': {'column_id': ['LG 진출', 'kt 진출']}, 'border-left': '2px solid black' , 'border-right': '4px solid black'},
+    {'if': {'column_id': ['(LG-kt)']}, 'border-right': '4px solid black'}],
     style_header = {'text-align': 'center', 'fontWeight': 'bold'},
     style_data = {'text-align': 'center', 'padding': '3px'},
     style_table={'margin-left': 'auto', 'margin-right': 'auto', 'margin-top': '10px', 'margin-bottom': '10px', 'width': '100%', 'max-width': '800px', 'overflowX': 'auto'}
 )
 
 # Playoff
-po_lg = {'(삼성-???)': 'LG'}
-po_lg.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items()})
-po_lg['삼성 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[0] == 3]), '.3%')
-po_lg['상대팀 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[1] == 3]), '.3%')
+po_initial = {'(삼성-LG)': '시리즈 초기'}
+po_initial.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items()})
+po_initial['삼성 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[0] == 3]), '.3%')
+po_initial['LG 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[1] == 3]), '.3%')
 
-po_kt = {'(삼성-???)': 'kt'}
-po_kt.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, log5(78/142, 72/142), 0, 0).items()})
-po_kt['삼성 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 72/142), 0, 0).items() if x[0] == 3]), '.3%')
-po_kt['상대팀 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 72/142), 0, 0).items() if x[1] == 3]), '.3%')
+po_now = {'(삼성-LG)': '현재'}
+po_now.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items()})
+po_now['삼성 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[0] == 3]), '.3%')
+po_now['LG 진출'] = format(sum([y for x, y in postseason_ratio(3, log5(78/142, 76/142), 0, 0).items() if x[1] == 3]), '.3%')
 
-po_probability = dash_table.DataTable([po_lg, po_kt],
-    [{'name': i, 'id': i} for i in ['(삼성-???)', '3-0', '3-1', '3-2', '삼성 진출', '0-3', '1-3', '2-3', '상대팀 진출']],
+po_probability = dash_table.DataTable([po_initial, po_now],
+    [{'name': i, 'id': i} for i in ['(삼성-LG)', '3-0', '3-1', '3-2', '삼성 진출', '0-3', '1-3', '2-3', 'LG 진출']],
     style_cell_conditional=[
-        {'if': {'column_id': ['삼성 진출', '상대팀 진출']}, 'border-left': '2px solid black' , 'border-right': '4px solid black'},
+        {'if': {'column_id': ['삼성 진출', 'LG 진출']}, 'border-left': '2px solid black' , 'border-right': '4px solid black'},
         {'if': {'column_id': ['(삼성-???)']}, 'border-right': '4px solid black'}],
     style_header = {'text-align': 'center', 'fontWeight': 'bold'},
     style_data = {'text-align': 'center', 'padding': '3px'},
@@ -306,12 +301,7 @@ ks_lg.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseas
 ks_lg['KIA 우승'] = format(sum([y for x, y in postseason_ratio(4, log5(87/142, 76/142), 0, 0).items() if x[0] == 4]), '.3%')
 ks_lg['상대팀 우승'] = format(sum([y for x, y in postseason_ratio(4, log5(87/142, 76/142), 0, 0).items() if x[1] == 4]), '.3%')
 
-ks_kt = {'(KIA-???)': 'kt'}
-ks_kt.update({'-'.join(list(map(str, x))): format(y, ".3%") for x, y in postseason_ratio(4, log5(87/142, 72/142), 0, 0).items()})
-ks_kt['KIA 우승'] = format(sum([y for x, y in postseason_ratio(4, log5(87/142, 72/142), 0, 0).items() if x[0] == 4]), '.3%')
-ks_kt['상대팀 우승'] = format(sum([y for x, y in postseason_ratio(4, log5(87/142, 72/142), 0, 0).items() if x[1] == 4]), '.3%')
-
-ks_probability = dash_table.DataTable([ks_ss, ks_lg, ks_kt],
+ks_probability = dash_table.DataTable([ks_ss, ks_lg],
     [{'name': i, 'id': i} for i in ['(KIA-???)', '4-0', '4-1', '4-2', '4-3', 'KIA 우승', '0-4', '1-4', '2-4', '3-4', '상대팀 우승']],
     style_cell_conditional=[
         {'if': {'column_id': ['KIA 우승', '상대팀 우승']}, 'border-left': '2px solid black' , 'border-right': '4px solid black'},
